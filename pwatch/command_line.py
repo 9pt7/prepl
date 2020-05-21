0165ef83ce4cf61ce78cca55d31ca74767fc0023
Peter Thompson <peter.thompson92@gmail.com>
2020-05-21 13:37:03 -0300

    Updated command line parser

--- pwatch/command_line.py
+++ pwatch/command_line.py
@@ -2,14 +2,19 @@ import argparse
 from .watch_command import watch_command
 
 
+parser = argparse.ArgumentParser(
+    prog="pwatch",
+    usage="%(prog)s [-h] command [args ...]",
+    description="""
+    Autorun command on file change.
+    """,
+)
+parser.add_argument("command", help="the command to run")
+parser.add_argument("args", nargs="*", help="additional arguments for command")
+
+
 def main(args=None):
-    parser = argparse.ArgumentParser(
-        description="""
-    Autorun commannd on file change
-    """
-    )
-    parser.add_argument("command")
-    parser.add_argument("args", nargs="*")
+
     args = parser.parse_args(args)
     try:
         watch_command([[args.command] + args.args])
