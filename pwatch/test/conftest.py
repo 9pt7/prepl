e424dbb4435aa83012f28b946868a777af8c9572
Peter Thompson <peter.thompson92@gmail.com>
2020-05-26 22:10:50 -0300

    Refactored watch test code

--- pwatch/test/conftest.py
+++ pwatch/test/conftest.py
@@ -21,31 +21,3 @@ def unwatched_file(tmp_path):
     unwatched_file = tmp_path / "unwatched_file.txt"
     unwatched_file.touch()
     return unwatched_file
-
-
-@pytest.fixture
-def subdir1(tmp_path):
-    subdir1 = tmp_path / "subdir1"
-    subdir1.mkdir()
-    return subdir1
-
-
-@pytest.fixture
-def subdir1_file1(subdir1):
-    subdir1_file1 = subdir1 / "file1.txt"
-    subdir1_file1.touch()
-    return subdir1_file1
-
-
-@pytest.fixture
-def subdir2(tmp_path):
-    subdir2 = tmp_path / "subdir2"
-    subdir2.mkdir()
-    return subdir2
-
-
-@pytest.fixture
-def subdir2_file1(subdir2):
-    subdir2_file1 = subdir2 / "file1.txt"
-    subdir2_file1.touch()
-    return subdir2_file1
--- pwatch/test/test_watch.py
+++ pwatch/test/test_watch.py
@@ -30,8 +30,8 @@ def test_modify_file(watch, watched_file):
     check_events(watch)
 
 
-def test_modify_unwatched_in_same_dir_as_watched(watch, watched_file, unwatched_file):
-    # Should not get event because path2 not being watched
+def test_modify_unwatched(watch, watched_file, unwatched_file):
+    # Should not get event because unwatched file not being watched
     modify(unwatched_file)
     check_no_events(watch)
 
@@ -42,34 +42,25 @@ def test_unlink(watch, watched_file):
     check_events(watch)
 
 
+def test_unlink_unwatched(watch, watched_file, unwatched_file):
+    # Check for event after unlinking
+    unwatched_file.unlink()
+    check_no_events(watch)
+
+
 def test_event_on_close(watch, watched_file):
     with open(watched_file, "wb", buffering=0) as f:
-        check_no_events(watch)
-
         f.write(b"hello world")
-
         check_no_events(watch)
-
     check_events(watch)
 
 
-def test_remove_dir(watch, subdir1, subdir1_file1):
-    watch.watch(subdir1_file1)
-    shutil.rmtree(subdir1)
+def test_remove_dir(watch, tmp_path, watched_file):
+    shutil.rmtree(tmp_path)
     check_events(watch)
 
 
-def test_move_replace(watch, watched_file, unwatched_file):
-    watch.watch(watched_file)
+def test_replace_watched(watch, watched_file, unwatched_file):
     check_no_events(watch)
-
     unwatched_file.rename(watched_file)
-    check_events(watch)
-
-
-def test_move_replace_from_other_dir(watch, subdir1_file1, subdir2_file1):
-    watch.watch(subdir1_file1)
-    check_no_events(watch)
-
-    subdir2_file1.rename(subdir1_file1)
     check_events(watch)
