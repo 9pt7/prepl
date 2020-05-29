395c975f458c67554b63787fe6d9334de5258549
Peter Thompson <peter.thompson92@gmail.com>
2020-05-29 11:29:11 -0300

    Added setuptools_scm for version

--- setup.py
+++ setup.py
@@ -1,36 +1,12 @@
 from skbuild import setup
 from setuptools import find_packages
-import subprocess
-from pathlib import Path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-rev_commit = (
-    subprocess.check_output(["git", "rev-list", "--tags", "--max-count=1"])
-    .decode("utf8")
-    .strip()
-)
-version = (
-    subprocess.check_output(["git", "describe", "--tags", rev_commit])
-    .decode("utf8")
-    .strip()
-)
-
-
-def test_path(path):
-    path = Path(path)
-    return path.suffix == "so" or path.name == "test_helper"
-
-
-def cmake_process_manifest_hook(file_list):
-    return filter(test_path, file_list)
-
-
 setup(
     name="pwatch",
-    version=version,
     author="Peter Thompson",
     author_email="peter.thompson92@gmail.com",
     description="Rerun command on source change",
@@ -46,5 +22,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
-    cmake_process_manifest_hook=cmake_process_manifest_hook,
+    use_scm_version=True,
 )
