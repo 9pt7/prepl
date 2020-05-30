8cf302027602adca345801b855e86548c578aeb6
Peter Thompson <peter.thompson92@gmail.com>
2020-05-30 11:22:33 -0300

    Set minimum Python version to Python 3.5
    The github CI can not handle 3.4

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.4",
+    python_requires=">=3.5",
     use_scm_version=True,
 )
