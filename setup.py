ea33f88aa9f2e2066122b71b54219dd99b94038a
Peter Thompson <peter.thompson92@gmail.com>
2020-05-30 11:21:02 -0300

    Attempt minimum Python version of 3.4

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.4",
     use_scm_version=True,
 )
