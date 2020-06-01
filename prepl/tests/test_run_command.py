7eff693f2efe962576319913b0844bfa1cf8f2fa
Peter Thompson <peter.thompson92@gmail.com>
2020-06-01 10:13:54 -0300

    Reenabled some commented out tests

--- prepl/tests/test_run_command.py
+++ prepl/tests/test_run_command.py
@@ -29,31 +29,31 @@ def test_editcmd(editcmd, unwatched_file):
     assert FileEvent(str(Path(unwatched_file).resolve()), False) in events
 
 
-# def test_cat(unwatched_file):
-#     events = run_command(["cat", str(unwatched_file)])
-#     assert FileEvent(str(unwatched_file), True) in events
+def test_cat(unwatched_file):
+    events = run_command(["cat", str(unwatched_file)])
+    assert FileEvent(str(Path(unwatched_file).resolve()), True) in events
 
 
-# def test_touch(unwatched_file):
-#     events = run_command(["touch", str(unwatched_file)])
-#     assert FileEvent(str(unwatched_file), False) in events
+def test_touch(unwatched_file):
+    events = run_command(["touch", str(unwatched_file)])
+    assert FileEvent(str(Path(unwatched_file).resolve()), False) in events
 
 
-# def test_stat(unwatched_file):
-#     events = run_command(["stat", "-L", str(unwatched_file)])
-#     assert FileEvent(str(unwatched_file), True) in events
+def test_stat(unwatched_file):
+    events = run_command(["stat", "-L", str(unwatched_file)])
+    assert FileEvent(str(Path(unwatched_file).resolve()), True) in events
 
 
-# def test_lstat(unwatched_file):
-#     events = run_command(["stat", str(unwatched_file)])
-#     assert FileEvent(str(unwatched_file), True) in events
+def test_lstat(unwatched_file):
+    events = run_command(["stat", str(unwatched_file)])
+    assert FileEvent(str(Path(unwatched_file).resolve()), True) in events
 
 
-# def test_unlink(unwatched_file):
-#     events = run_command(["unlink", str(unwatched_file)])
-#     assert FileEvent(str(unwatched_file), False) in events
+def test_unlink(unwatched_file):
+    events = run_command(["unlink", str(unwatched_file)])
+    assert FileEvent(str(Path(unwatched_file).resolve()), False) in events
 
 
-# def test_rm_r(tmp_path, unwatched_file):
-#     events = run_command(["rm", "-r", str(tmp_path)])
-#     assert FileEvent(str(unwatched_file), False) in events
+def test_rm_r(tmp_path, unwatched_file):
+    events = run_command(["rm", "-r", str(tmp_path)])
+    assert FileEvent(str(Path(unwatched_file).resolve()), False) in events
